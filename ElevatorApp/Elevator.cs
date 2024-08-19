8302ecaed422bed958ca151c82724ea7fc1ccfd1
Steve <stevenosan@gmail.com>
2024-08-19 15:11:14 -0500

    Add project files.

---
+++ ElevatorApp/Elevator.cs
@@ -0,0 +1,31 @@
+﻿namespace ElevatorApp;
+public class Elevator
+{
+    public int CurrentFloor = 1;
+    public int TopFloor => 10;
+    public List<int> Passengers = [];
+
+    public List<Floor> Floors = Enumerable.Range(1, 10).Select(x => new Floor(x)).ToList();
+}
+
+public class WaitingPassenger(int desiredFloor)
+{
+    public int DesiredFloor;
+}
+
+public enum Direction
+{
+    Up,
+    Down,
+}
+
+public class Floor
+{
+    public int FloorNumber;
+    public Queue<WaitingPassenger> WaitingPassengers = [];
+
+    public Floor(int floorNumber)
+    {
+        FloorNumber = floorNumber;
+    }
+}
---
+++ ElevatorApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ElevatorApp;
+
+var elevator = new Elevator();
+
+
+Console.WriteLine($"The elevator is currently on floor {elevator.CurrentFloor}. Please enter a direction. U for Up, D for down. Enter X to exit the simulation.");
+var direction = Console.ReadLine();
+while (direction != "X")
+{
+    //if(elevator.)
+
+    //var passenger = new Passenger()
+}
+
+Console.WriteLine("The simulation has ended.");
+
