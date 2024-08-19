fc66c9ccdd463d66e1f285cd86b1f35e9d990b0a
Steve <stevenosan@gmail.com>
2024-08-19 16:07:51 -0500

    basic start to app

--- ElevatorApp/Elevator.cs
+++ ElevatorApp/Elevator.cs
@@ -3,12 +3,22 @@ public class Elevator
 {
     public int CurrentFloor = 1;
     public int TopFloor => 10;
-    public List<int> Passengers = [];
+    public int Time = 0;
+    public Direction Direction;
+    public List<Passenger> Passengers = [];
 
     public List<Floor> Floors = Enumerable.Range(1, 10).Select(x => new Floor(x)).ToList();
+
+    public List<PendingRequest> PendingRequests = new List<PendingRequest>();
 }
 
-public class WaitingPassenger(int desiredFloor)
+public class PendingRequest
+{
+    public int Floor;
+    public Direction RequestedDirection;
+}
+
+public class Passenger(int desiredFloor)
 {
     public int DesiredFloor;
 }
@@ -22,7 +32,7 @@ public enum Direction
 public class Floor
 {
     public int FloorNumber;
-    public Queue<WaitingPassenger> WaitingPassengers = [];
+    public Passenger WaitingPassenger;
 
     public Floor(int floorNumber)
     {
--- ElevatorApp/Program.cs
+++ ElevatorApp/Program.cs
@@ -5,13 +5,36 @@ using ElevatorApp;
 var elevator = new Elevator();
 
 
-Console.WriteLine($"The elevator is currently on floor {elevator.CurrentFloor}. Please enter a direction. U for Up, D for down. Enter X to exit the simulation.");
-var direction = Console.ReadLine();
-while (direction != "X")
+Console.WriteLine($"The elevator is currently on floor {elevator.CurrentFloor}. Please enter the floor of the current passenger (1-10). Enter X to exit the simulation:");
+var command = Console.ReadLine();
+while (command != "X")
 {
-    //if(elevator.)
+    var passengerFloor = int.Parse(command);
 
-    //var passenger = new Passenger()
+    //create pending request
+    elevator.PendingRequests.Add(new PendingRequest { RequestedDirection = Direction.Up, Floor = passengerFloor });
+
+    //disembark
+    elevator.Passengers.RemoveAll(p => p.DesiredFloor == elevator.CurrentFloor);
+
+    //embark
+    Console.WriteLine("You have entered the elevator. Please enter your desired floor (1-10):");
+    var desiredFloor = int.Parse(Console.ReadLine());
+
+    var passenger = new Passenger(desiredFloor);
+    elevator.Passengers.Add(passenger);
+
+    //end time
+    var nextDesiredFloor = elevator.Passengers.FirstOrDefault().DesiredFloor;
+    var nextRequestedPickup = elevator.PendingRequests.FirstOrDefault().Floor;
+
+    if(nextDesiredFloor > elevator.CurrentFloor)
+    {
+        Console.WriteLine("Going up!");
+        elevator.Direction = Direction.Up;
+    }
+
+    Console.WriteLine($"Passenger has embarked. Time is currently {elevator.Time}. Direction: {elevator.Direction}. Current Floor: {elevator.CurrentFloor}");
 }
 
 Console.WriteLine("The simulation has ended.");
