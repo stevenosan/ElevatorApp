ebc98308b55fe5cb73f4f59ae99e9d7af3e8328e
Steve Nosan <stephen.nosan@insight.com>
2024-08-19 20:05:33 -0500

    refactoring efforts

--- ElevatorApp/Elevator.cs
+++ ElevatorApp/Elevator.cs
@@ -4,12 +4,42 @@ public class Elevator
     public int CurrentFloor = 1;
     public int TopFloor => 10;
     public int Time = 0;
-    public Direction Direction = Direction.Up;
+    public Direction Direction => calculateDirection();
     public List<Passenger> Passengers = [];
 
     public List<Floor> Floors = Enumerable.Range(1, 10).Select(x => new Floor(x)).ToList();
 
     public List<PendingRequest> PendingRequests = new List<PendingRequest>();
+
+    private Direction calculateDirection()
+    {
+        var nextFloorVal = Passengers.FirstOrDefault(p => p.DesiredFloor > CurrentFloor)?.DesiredFloor;
+
+        var nextPickupVal = PendingRequests.FirstOrDefault()?.Floor;
+        var nextRequestedPickupFloor = nextPickupVal ??= 0;
+
+        return nextFloorVal > nextRequestedPickupFloor && nextRequestedPickupFloor < CurrentFloor ? Direction.Up : Direction.Down;
+    }
+
+    private int calculateNextDestinationFloor()
+    {
+
+    }
+
+    public void DisembarkPassengers()
+    {
+
+    }
+
+    public void EmbarkPassenger()
+    {
+
+    }
+
+    public void RequestElevator()
+    {
+
+    }
 }
 
 public class PendingRequest
